af2c68689051a116b69871eda27c8721f3bcc2e3
venetucci <michelle.ann.harvey@gmail.com>
2016-04-16 17:35:11 -0700

    Add pan gesture and color changes to card

--- FlashcardViewController.swift
+++ FlashcardViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SwiftHEXColors
 
 class FlashcardViewController: UIViewController {
     
@@ -17,12 +18,27 @@ class FlashcardViewController: UIViewController {
     var front: UIView!
     var back: UIView!
     
+    let blue = UIColor(hexString: "#43DAF8")!
+    let green = UIColor(hexString: "#7AEA7A")!
+    let red = UIColor(hexString: "#F96B54")!
+    let white = UIColor(hexString: "FFFFFF")!
+    let black = UIColor(hexString: "171616")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         flashcardStyles()
         
-        backgroundView.backgroundColor = UIColor.lightGrayColor()
+        backgroundView.backgroundColor = blue
+        
+        // The onCustomPan: method will be defined in Step 3 below.
+        var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onCustomPan:")
+       
+//        cardStart = CGPoint(x: cardView.center.x, y: cardView.center.y + cardDownOffset)
+        
+        // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+        cardView.userInteractionEnabled = true
+        cardView.addGestureRecognizer(panGestureRecognizer)
     }
     
     func tapped() {
@@ -40,31 +56,78 @@ class FlashcardViewController: UIViewController {
         
     }
     
+    func onCustomPan(sender: UIPanGestureRecognizer) {
+        let point = sender.locationInView(view)
+        let velocity = sender.velocityInView(view)
+        let translation = sender.translationInView(view)
+        
+        var cardInitialCenter: CGPoint!
+        
+        cardInitialCenter = cardView.center
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            cardView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y)
+            self.cardView.transform = CGAffineTransformMakeRotation(CGFloat(translation.x * 15 / 160 * CGFloat(M_PI) / 180))
+
+            print("Gesture changed at: \(point)")
+            print("center.x: \(cardInitialCenter.x + translation.x)")
+            
+            if point.x > 150 {
+                UIView.animateWithDuration(0.4, animations: {
+                    self.backgroundView.backgroundColor = self.green
+                })
+            } else if point.x < 100 {
+                UIView.animateWithDuration(0.4, animations: {
+                    self.backgroundView.backgroundColor = self.red
+                })
+            }
+
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            if translation.x > -40 && translation.x < 50 {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.cardView.center.x = 160
+                    self.cardView.transform = CGAffineTransformIdentity
+                })
+            } else if translation.x > 51 {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.cardView.center.x = 1000
+                })
+            } else {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.cardView.center.x = -1000
+                })
+            }
+            print("Gesture ended at: \(point)")
+        }
+    }
+    
     override func viewWillLayoutSubviews() {
         cardView.center = view.center
     }
 
     func flashcardStyles() {
         let width = self.view.frame.width * 0.85
-        let height = self.view.frame.height * 0.85
-        let y = self.view.frame.minY - 10.0
+        let height = self.view.frame.height * 0.55
+        let y = self.view.frame.minY
         let rect = CGRectMake(0, y, width, height)
         
         let singleTap = UITapGestureRecognizer(target: self, action: "tapped")
         singleTap.numberOfTapsRequired = 1
         
         front = UIView(frame: rect)
-        front.backgroundColor = UIColor.whiteColor()
+        front.backgroundColor = white
         front.layer.cornerRadius = 16
-        front.layer.shadowColor = UIColor.blackColor().CGColor
+        front.layer.shadowColor = black.CGColor
         front.layer.shadowOpacity = 0.2
         front.layer.shadowOffset = CGSizeZero
         front.layer.shadowRadius = 16
         
         back = UIView(frame: rect)
-        back.backgroundColor = UIColor.whiteColor()
+        back.backgroundColor = white
         back.layer.cornerRadius = 16
-        back.layer.shadowColor = UIColor.blackColor().CGColor
+        back.layer.shadowColor = black.CGColor
         back.layer.shadowOpacity = 0.2
         back.layer.shadowOffset = CGSizeZero
         back.layer.shadowRadius = 16
@@ -74,6 +137,27 @@ class FlashcardViewController: UIViewController {
         cardView.userInteractionEnabled = true
         cardView.addSubview(back)
         self.view.addSubview(cardView)
+        
+        // card content
+        
+        let textWidth = self.view.frame.width - 110
+        let textHeight = self.view.frame.height - 60
+        let textY = self.view.frame.minY + 30
+        let textX = self.view.frame.minX + 30
+        let bodyLabel = UILabel(frame: CGRectMake(textX, textY, textWidth, textHeight))
+        let bodyFontOne = UIFont(name:"Avenir", size: 28.0)
+        
+        bodyLabel.text = "sample text here this is a whole sentence"
+        bodyLabel.textColor = black
+        bodyLabel.font = bodyFontOne
+        bodyLabel.textAlignment = NSTextAlignment.Center
+        bodyLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        bodyLabel.numberOfLines = 0
+        bodyLabel.contentMode = UIViewContentMode.ScaleAspectFit
+        bodyLabel.sizeToFit()
+        cardView.addSubview(bodyLabel)
+        
+        let bodyHeight = bodyLabel.bounds.height
         
     }
     
